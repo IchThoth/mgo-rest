244fe7d9a3fd19b69b0821d4cbe0c64208377a30
ichthoth <shalomtata334@gmail.com>
2022-07-05 22:00:25 +0100

    finished addind packages

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2" 
+	"net/http"
+
+	"github.com/IchThoth/Go-MongoDB-REST-API/controllers"
+)
+
 func main() {
 
 }
