61893473cb765e1ed60796c945572eabb2b7dcd9
ichthoth <shalomtata334@gmail.com>
2022-07-05 22:15:12 +0100

    added r variable and connected to mongodb client

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
 
+	"github.com/IchThoth/Go-MongoDB-REST-API/controllers"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+	r.GET("")
+	r.POST("")
+	r.DELETE("")
+}
+
+func getSession() *mgo.Session {
+	//connects to mongodb client//
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
 }
