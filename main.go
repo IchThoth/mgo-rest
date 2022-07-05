f83cf542193f7e0b9498006fb48e39850f95857a
ichthoth <shalomtata334@gmail.com>
2022-07-05 22:07:08 +0100

    created r variable and http requests

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"github.com/julienschmidt/httprouter"
-	"gopkg.in/mgo.v2" 
-	"net/http"
-
-	"github.com/IchThoth/Go-MongoDB-REST-API/controllers"
-)
-
 func main() {
 
 }
